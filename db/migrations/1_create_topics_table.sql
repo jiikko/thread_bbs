055ec0716a6d1d52cd8289b6a9d0fe7b29ec802c
jiikko <n905i.1214@gmail.com>
2019-05-01 14:13:52 +0900

    migrationファイルを追加する

---
+++ db/migrations/1_create_topics_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `topics_tables` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `body` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC
+;
