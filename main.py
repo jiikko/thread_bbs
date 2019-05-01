6f7bbf54f790a69e53072880eae988b898f9be8c
jiikko <n905i.1214@gmail.com>
2019-05-01 12:46:58 +0900

    formからpostできるようにする

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from flask import Flask, render_template, request
+import logging
 app = Flask(__name__)
+
 @app.route('/form')
 def form():
     return render_template('form.html')
+
+@app.route('/')
+def top():
+    return render_template('top.html')
+
+@app.route('/topics/new')
+def topics_new():
+    return render_template('topics/new.html')
+
+@app.route('/topics', methods=['POST'])
+def topics_create():
+    logging.info('posted topics')
+    return render_template('topics/new.html')
