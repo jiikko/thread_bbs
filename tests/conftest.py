ea359234e28cd3dc66a5f99e7b13079e6d2aefb8
jiikko <n905i.1214@gmail.com>
2019-05-03 01:45:44 +0900

    テスト実行前にレコードを削除する

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,5 +5,10 @@ import application
 def client():
     app = application.app
     app.testing = True
+    clean_tables
     with app.test_client() as client:
         yield client
+
+def clean_tables():
+    with application.rdb.conn() as cursor:
+        cursor.execute('truncate table topics')
