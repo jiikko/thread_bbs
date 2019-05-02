efa4b72e1da20186e093d622f8d6a228f70ab3fc
jiikko <n905i.1214@gmail.com>
2019-05-02 16:56:27 +0900

    pytest.fixuteを使ってテストの前処理を共通化する

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+import application
+
+@pytest.fixture
+def client():
+    app = application.app
+    app.testing = True
+    with app.test_client() as client:
+        yield client
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,6 @@
 import pytest
 import application
 
-@pytest.fixture
-def main():
-    pass
-
-def test_app():
-    app = application.app
-    app.testing = True
-    client = app.test_client()
+def test_app(client):
     response = client.get('/')
     assert response.status_code == 200
