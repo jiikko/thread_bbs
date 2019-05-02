79263ccde0da909277863ba3dbe9c49948c0b10a
jiikko <n905i.1214@gmail.com>
2019-05-02 13:04:53 +0900

    ファイル変更検出してプロセス再起動の無視リストを更新する

--- bin/server.sh
+++ bin/server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-yes | dev_appserver.py --watcher_ignore_re='(.*mysql)' --env_var MYSQL_HOST=$MYSQL_HOST --host=0.0.0.0 .
+yes | dev_appserver.py --watcher_ignore_re='(.*mysql|^/app/bin/.*|.*/tests/.*)' --env_var MYSQL_HOST=$MYSQL_HOST --host=0.0.0.0 .
