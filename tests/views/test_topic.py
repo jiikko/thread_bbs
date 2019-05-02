e27617d46d4189d6e7c9a59dde71539b1b845bca
jiikko <n905i.1214@gmail.com>
2019-05-02 14:32:17 +0900

    test_clientをwithで使ってみる

--- tests/views/test_topic.py
+++ tests/views/test_topic.py
@@ -18,11 +18,11 @@ def test_post_topics_new_when_invalid():
 def test_post_topics_new():
     app = application.app
     app.testing = True
-    client = app.test_client()
-    response = client.post('/topics/new', data={ 'title': "nodogaitai_11", 'body': "nodogaitai_11" })
-    assert response.status_code == 302
-    response = client.get('/topics/')
-    assert 'nodogaitai_11' in str(response.get_data())
+    with app.test_client() as c:
+        response = c.post('/topics/new', data={ 'title': "nodogaitai_11", 'body': "nodogaitai_11" })
+        assert response.status_code == 302
+        response = c.get('/topics/')
+        assert 'nodogaitai_11' in str(response.get_data())
 
 def test_get_topics_index():
     app = application.app
