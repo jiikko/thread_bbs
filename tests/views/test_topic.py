9f785fda6b8d82b4817c9c78bcde08d8569592ee
jiikko <n905i.1214@gmail.com>
2019-05-03 12:07:07 +0900

    topicを編集するテストコードを追加する

--- tests/views/test_topic.py
+++ tests/views/test_topic.py
@@ -1,6 +1,9 @@
 import pytest
 import application
 
+def test_get_topics_index(client):
+    response = client.get('/topics/')
+    assert response.status_code == 200
 
 def test_get_topics_new(client):
     response = client.get('/topics/new')
@@ -18,6 +21,38 @@ def test_post_topics_new(client):
     actual = response.get_data()
     assert expected in actual
 
-def test_get_topics_index(client):
-    response = client.get('/topics/')
+def test_get_topics_edit(client):
+    title = 'test_get_topics_edit_title'
+    body = 'test_get_topics_edit_body'
+    application.rdb.insert_topics(title=title, body=body)
+    topic_id = None
+    with application.rdb.conn() as cursor:
+        cursor.execute("select * from topics where title = %s", [title])
+        row = cursor.fetchall()[0]
+        topic_id = row[0]
+
+    response = client.get('/topics/%s/edit' % topic_id)
     assert response.status_code == 200
+    actual = response.get_data()
+    assert title in actual
+    assert body in actual
+
+def test_post_topics_edit(client):
+    title = 'test_get_topics_edit_title'
+    body = 'test_get_topics_edit_body'
+    application.rdb.insert_topics(title=title, body=body)
+    topic_id = None
+    with application.rdb.conn() as cursor:
+        cursor.execute("select * from topics where title = %s", [title])
+        row = cursor.fetchall()[0]
+        topic_id = row[0]
+    response = client.post('/topics/%s/edit' % topic_id, data={ 'title': 'titletitle', 'body': 'bodybody' })
+    assert response.status_code == 302
+
+    row = None
+    with application.rdb.conn() as cursor:
+        cursor.execute("select * from topics where id = %s", [topic_id])
+        row = cursor.fetchall()[0]
+
+    assert 'titletitle' == row[1]
+    assert 'bodybody' == row[2]
